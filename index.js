99039a2abdda344821f354b470c22a41acc38abb
João Vitor Alves Muniz <83318101+JoaoVitorMuniz@users.noreply.github.com>
2021-05-18 20:14:43 -0300

    testando
    testando

---
+++ index.js
@@ -0,0 +1,71 @@
+async function fetchPosts() {
+  const list = document.getElementById("list");
+
+  const result = await fetch("https://spacesecret.herokuapp.com/post");
+  const json = await result.json();
+  json.post.map((urso) => {
+    const item = `
+    <div class="item" onclick="fetchPost('${urso._id}')">
+    <div
+      style="
+        background-image: url('${urso.imagem}');
+      "
+    >
+      <h2 class="title">${urso.titulo}</h2>
+    </div>
+  </div>
+    `;
+
+    list.innerHTML += item;
+  });
+}
+fetchPosts();
+
+async function fetchPost(id) {
+  try {
+    const result = await fetch(`https://spacesecret.herokuapp.com/post/${id}`);
+    console.log(result);
+    if (!result.ok) {
+      alert("Post Indisponível");
+    }
+
+    const json = await result.json();
+
+    modal(json.post.titulo, json.post.desc, json.post.imagem,json.post._id);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function modal(titulo, desc, imagem,_id) {
+  document.getElementById("modalContainer").innerHTML = `
+  <div class="hero-img" style="background-image: url('${imagem}')"></div>
+  <div class="content">
+    <h1>${titulo}</h1>
+    <p>${desc}</p>
+    <button id="btnDeletar" onclick="deletPost('${_id}')"><img src="src/trash.svg"/></button>
+    <button id="btnCloseModal">X</button>
+  </div>
+  `;
+
+  document.getElementById("modalContainer").style.display = "flex";
+  document.getElementById("btnCloseModal").addEventListener("click", () => {
+    document.getElementById("modalContainer").style.display = "none";
+  });
+}
+
+async function deletPost (id){
+  
+  const result = await fetch("https://spacesecret.herokuapp.com/post/" + id , {
+     method: "delete",
+     headers: {
+         "Authorization": localStorage.getItem("idUser")     
+     }
+   })
+   if(!result.ok){
+       alert("Sem Permissao");
+       return;
+   }
+   
+   location.href = "home.html"
+}
---
+++ login.js
@@ -0,0 +1,30 @@
+document.getElementById("form").addEventListener("submit", async (e) => {
+  try {
+    e.preventDefault();
+    const email = document.getElementById("email").value;
+    const pass = document.getElementById("pass").value;
+    const body = JSON.stringify({ email, senha: pass });
+
+    const result = await fetch("https://spacesecret.herokuapp.com/login", {
+      method: "POST",
+      body,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const json = await result.json();
+
+    if (!result.ok) {
+      alert(json.error);
+      return;
+    }
+    
+
+    localStorage.setItem("idUser", json.usuario._id);
+
+    location.href = "/home.html";
+  } catch (err) {
+    console.log(err);
+  }
+});
